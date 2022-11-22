d797a8ad1f2b7494b501bf5581c279a1718a1dd4
Astrid Prins <35829@ma-web.nl>
2022-11-22 10:37:14 +0100

    les1 opdrachten

---
+++ les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello World!");
+        //code gaat hier!
+        let appNaam = "Les 1";
+        let versienummer = 0.9;
+        let versiedatum =  "22/11/2022";
+        let autheur = "Astrid Prins";
+        let copyright = "copyright";
+        let distributeur = "Meep Meep";
+        let darkmode = true;
+
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+}
+
+let app = new App();
+app.runApplication();
