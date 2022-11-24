ab8dea58dafe3a821e697c15611563f906147e4e
Astrid Prins <35829@ma-web.nl>
2022-11-24 13:15:06 +0100

    js house

---
+++ canvashuis/app.js
@@ -0,0 +1,80 @@
+class App
+{
+    runApplication()
+    {
+        //code gaat hier!
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        
+        console.log(canvas)
+        
+        // Lucht 
+        g.beginPath()
+        g.fillStyle = "#99ccff";
+        g.moveTo(0,0);
+        g.lineTo(0,250);
+        g.lineTo(1000,250);
+        g.lineTo(1000,0);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        // grass
+        g.beginPath()
+        g.fillStyle = "#00e600";
+        g.moveTo(0,250);
+        g.lineTo(0,700);
+        g.lineTo(1000,700);
+        g.lineTo(1000,250);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        // Groot dak vlak
+        g.beginPath()
+        g.fillStyle = "#cc0000";
+        g.moveTo(300,100);
+        g.lineTo(200,300);
+        g.lineTo(600,400);
+        g.lineTo(700,200);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        
+        // Driehoek dak vlak
+        g.beginPath()
+        g.fillStyle = "#990033";
+        g.moveTo(700,200);
+        g.lineTo(600,400);
+        g.lineTo(800,300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        // Groot muur vlak
+        g.beginPath()
+        g.fillStyle = "#ffcc99";
+        g.moveTo(200,300);
+        g.lineTo(200,500);
+        g.lineTo(600,600);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill()
+ 
+        // Klein muur vlak
+        g.beginPath()
+        g.fillStyle = "#ffb366"
+        g.moveTo(600,400);
+        g.lineTo(600,600);
+        g.lineTo(800,500);
+        g.lineTo(800,300);
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+    }
+}
+
+let app = new App();
+app.runApplication();
