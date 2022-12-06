fb4cf2659e2c38f60aad8163c229e6eb940f0c75
Astrid Prins <35829@ma-web.nl>
2022-12-06 10:56:23 +0100

    array and loops

--- functions/app.js
+++ functions/app.js
@@ -3,44 +3,44 @@ class App
     runApplication()
     {
         this.marioFunction();
-        this.laughFunction();
-        this.countFunction();
+        let laugh2 = this.laughFunction();
+        let count2 = this.countFunction();
         this.meFunction();
+
+        console.log(laugh2);
+        console.log(count2);
     }
     
     marioFunction()
-        {
-            let mario = "MARIO!!";
-            console.log(mario);
-        }
+    {
+        let mario = "MARIO!!";
+        console.log(mario);
+    }
 
     laughFunction()
-        {
-            let laugh = "BWAHAHA";
-            return laugh;
+    {
+        let laugh = "BWAHAHA";
+        return laugh;
 
-        }
+    }
 
     countFunction()
-        {
-            let num1 = 4;
-            let num2 = 4;
-            let count = num1 + num2;
-            return count;
+    {
+        let num1 = 4;
+        let num2 = 4;
+        let count = num1 + num2;
+        return count;
 
-        }
+    }
         
     meFunction()
-        {
-            let me = 6 * 6;
-            console.log(me);
-        }
+    {
+        let me = 6 * 6;
+        console.log(me);
+    }
+        
 }
 
 
 let app = new App();
 app.runApplication();
-app.marioFunction();
-app.laughFunction();
-app.countFunction();
-app.meFunction();
